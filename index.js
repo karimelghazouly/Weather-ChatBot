1c897d76bc3c112b046c2eecb7e4b21ba6b45c7f
Karim ElGhaazouly <karimelghazouly@gmail.com>
2018-05-25 13:09:39 +0200

    Facebook WebHock

---
+++ index.js
@@ -0,0 +1,43 @@
+var exp=require('express');
+var bp=require('body-parser');
+var app = exp();
+app.use(bp.json());
+app.use(bp.urlencoded({extended:true}));
+app.get('/',function(req,res){
+	res.send("el btngan el ahmr");
+
+})
+
+app.post('/webhook', (req, res) => {  
+ 
+  let body = req.body;
+
+  if (body.object === 'page') {
+    body.entry.forEach(function(entry) {
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+    });
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    res.sendStatus(404);
+  }
+
+});
+
+app.get('/webhook', (req, res) => {
+  let VERIFY_TOKEN = "ghazouly"
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+  if (mode && token) {
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    
+    } else {
+      res.sendStatus(403);      
+    }
+  }
+});
+
+app.listen(3000, () => console.log('Webhook server is listening, port 3000'));
