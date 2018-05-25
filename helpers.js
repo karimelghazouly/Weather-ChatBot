9869353d4bbde675a8d9df6d5d3fcb4fb0f1bebf
Karim ElGhaazouly <karimelghazouly@gmail.com>
2018-05-25 22:28:25 +0200

    changes

--- helpers.js
+++ helpers.js
@@ -2,6 +2,21 @@ const ai_token='9cad650e13084baea6efc0fca668b402';
 var app = require('apiai')(ai_token);
 var request = require('request');
 var fb_token='2ecd56ce6c9388693566b68af63b443b';
+exports.SendResponse=function(txt,idx)
+{
+	var send=function(idx,txt){
+		request({
+			url:'https://graph.facebook.com/v2.6/me/messages',
+			 qs: { access_token: fb_token },
+			 method: 'POST',
+			 json: {
+			 recipient: { id: idx },
+			 message: { txt },
+			 }
+		});
+	}
+}
+
 exports.SendText=function(txt,id)
 {
 	console.log("sending text to api ai");
@@ -29,18 +44,4 @@ exports.SendText=function(txt,id)
 	    console.log(error);
 	});
 	req.end();
-}
-exports.SendResponse=function(txt,idx)
-{
-	var send=function(idx,txt){
-		request({
-			url:'https://graph.facebook.com/v2.6/me/messages',
-			 qs: { access_token: fb_token },
-			 method: 'POST',
-			 json: {
-			 recipient: { id: idx },
-			 message: { txt },
-			 }
-		});
-	}
 }
