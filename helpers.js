47d915eea315ca10ce7aee0766cd9f826ac0cc33
Karim ElGhaazouly <karimelghazouly@gmail.com>
2018-05-25 16:11:38 +0200

    changes

---
+++ helpers.js
@@ -0,0 +1,14 @@
+const ai_token='9cad650e13084baea6efc0fca668b402';
+var app = require('apiai');
+exports.SendText=function(txt)
+{
+	var req=app.textRequest(txt,{
+		sessionid:'hamada'
+	})
+	request.on('response', function(response) {
+    console.log(response);
+	});
+	request.on('error', function(error) {
+	    console.log(error);
+	});
+}
