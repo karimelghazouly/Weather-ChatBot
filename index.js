0249e83f36dc8736e65ccad2935cabbabc87fcaf
Karim ElGhaazouly <karimelghazouly@gmail.com>
2018-05-25 16:41:23 +0200

    changes

--- index.js
+++ index.js
@@ -49,4 +49,4 @@ app.get('/webhook', (req, res) => {
   }
 });
 
-app.listen(process.env.PORT || 3000, () => console.log('Webhook server is listening, port 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('webhook is running'));
