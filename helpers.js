deb5a9995a129822f69d7d0721b5f81d8f93d848
Karim ElGhaazouly <karimelghazouly@gmail.com>
2018-05-26 13:54:48 +0200

    Supporting weather by coordinates

--- helpers.js
+++ helpers.js
@@ -31,22 +31,22 @@ function GetWeatherByCityName(id) {
   SendResponse(id,last);
 
 });
-
- function GetWeatherByCoord(id) {
- request('http://api.openweathermap.org/data/2.5/weather?lat='+lat+'lon='+long+'&APPID=ba25db8f381ce66103009bf7b240c530', function (error, response, body) {
-  console.log('error:', error); 
-  console.log('statusCode:', response && response.statusCode); 
-  console.log('body:',body);
-  var c=JSON.parse(body);
-  var temp=JSON.stringify(c.main.temp);
-  var desc=JSON.stringify(c.weather[0].description);
-  var last ="description:"+desc+" with temperature : "+temp;
-  console.log("last  = "+last);
-  SendResponse(id,last);
-
-});
+}
  
-};
+function GetWeatherByCoord(id) {
+	request('http://api.openweathermap.org/data/2.5/weather?lat='+lat+'lon='+long+'&APPID=ba25db8f381ce66103009bf7b240c530', function (error, response, body) {
+	console.log('error:', error); 
+	console.log('statusCode:', response && response.statusCode); 
+	console.log('body:',body);
+	var c=JSON.parse(body);
+	var temp=JSON.stringify(c.main.temp);
+	var desc=JSON.stringify(c.weather[0].description);
+	var last ="description:"+desc+" with temperature : "+temp;
+	console.log("last  = "+last);
+	SendResponse(id,last);
+});
+}
+ 
 exports.SendText=function(txt,id)
 {
 	console.log("------------------------>sending text to api ai");
