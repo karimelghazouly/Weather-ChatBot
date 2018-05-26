536952dcc4753cfc7dd0aaa744234a7d4bc50a3e
Karim ElGhaazouly <karimelghazouly@gmail.com>
2018-05-26 02:27:02 +0200

    changes

--- helpers.js
+++ helpers.js
@@ -23,7 +23,8 @@ const GetWeatherByCityName = () => {
   console.log('error:', error); 
   console.log('statusCode:', response && response.statusCode); 
   var c=JSON.parse(body);
-  console.log('main = '+c.main.toString());
+  var s=c.main.toString();
+  console.log('s = '+s);
   console.log('coord = '+c.coord.toString());
   console.log("body : "+body);
   console.log("main : "+body[c]);
