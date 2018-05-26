6f7c4b497b3c97d38d8b0be7c497f46612f5124b
Karim ElGhaazouly <karimelghazouly@gmail.com>
2018-05-26 02:52:47 +0200

    changes

--- helpers.js
+++ helpers.js
@@ -19,10 +19,10 @@ const SendResponse = (senderId, text) => {
  });
  console.log("done");
 };
-const GetWeatherByCityName = () => {
+function GetWeatherByCityName() {
  request('http://api.openweathermap.org/data/2.5/weather?q='+cityname+'&APPID=ba25db8f381ce66103009bf7b240c530', function (error, response, body) {
-  console.log('error:', error); 
-  console.log('statusCode:', response && response.statusCode); 
+  //console.log('error:', error); 
+  //console.log('statusCode:', response && response.statusCode); 
   var c=JSON.parse(body);
   var temp=JSON.stringify(c.main.temp);
   var desc=JSON.stringify(c.weather[0].description);
