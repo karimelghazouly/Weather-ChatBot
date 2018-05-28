d960a1f10364d8d0cac12aa1c54edc19ab67fb44
Karim ElGhaazouly <karimelghazouly@gmail.com>
2018-05-28 18:15:21 +0200

    mongo db

---
+++ db.js
@@ -0,0 +1,32 @@
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://karimelghazouly:1234567gg@ds237610.mlab.com:37610/users';
+const dbName = 'users';
+const objj = { name: "Testing user", FBID: "123123" };
+exports.function conn(op,obj,fun)
+{
+	MongoClient.connect(url, function(err, db) {
+	  if (err) throw err;
+	  var dbo = db.db("users");
+	  if(op=='f')
+	  {
+	  	dbo.collection("users").findOne(obj, function(err, result) {
+		    if (err) throw err;
+		    console.log("result",result);
+		    fun(result);
+		    db.close();
+	  	});	
+	  }
+	  else if(op=='i')
+	  {
+	  	dbo.collection("users").insertOne(obj, function(err, res) {
+		    if (err) throw err;
+		    console.log("insertion complete");
+		    fun();
+		    db.close();
+		});
+	  }
+	  else db.close();
+
+	});
+
+}
