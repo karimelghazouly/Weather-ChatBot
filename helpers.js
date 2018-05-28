6e911a32a0159c14c199fdc1969c7670b953b68a
Karim ElGhaazouly <karimelghazouly@gmail.com>
2018-05-28 21:43:37 +0200

    mongo db

--- helpers.js
+++ helpers.js
@@ -101,10 +101,11 @@ exports.SendText=function(txt,id,result)
     			if(cityname!='')
     			{
 					DB.conn('u',{id:id},{$set:{id:id,city:cityname}},function(){});
+					SendResponse(id,"Great now i can help you with the weather in any area in the world , i can also help you to choose what to wear for your outings , how can you do this ? just type your question like what should i wear or what is the weather in any city/Country in the world");
 			    }
     			else
     			{
-    				// ab3tlo massage zan
+    				SendResponse(id,"Please tell me your city");
     			}
     		}
     	}
